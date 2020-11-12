59b37868983c6b8663ae8f3d50d96cc08b8c26c9
Delvector21 <delvector@hotmail.com>
2020-11-12 12:37:35 -0300

    Agregar Base
    Agregamos dependecias de volley y picasso
    tablayout
    generamos los framentos personajes y ubicacion

---
+++ app/src/main/java/cl/inacap/rickmorty/fragments/UbicacionesFragment.java
@@ -0,0 +1,33 @@
+package cl.inacap.rickmorty.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.inacap.rickmorty.R;
+
+
+public class UbicacionesFragment extends Fragment {
+
+
+    public UbicacionesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ubicaciones, container, false);
+    }
+}
